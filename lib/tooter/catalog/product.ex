17073a10651f315e2bdd1c72a1778c3f5a684b59
x123 <x123@users.noreply.github.com>
2024-09-09 12:44:29 +0200

    add product changeset to reduce price

--- lib/tooter/catalog/product.ex
+++ lib/tooter/catalog/product.ex
@@ -19,4 +19,15 @@ defmodule Tooter.Catalog.Product do
     |> unique_constraint(:sku)
     |> validate_number(:unit_price, greater_than: 0.0)
   end
+
+  @doc false
+  def changeset_reduce_price(product, new_price) do
+    attrs = %{unit_price: new_price}
+    old_price = product.unit_price
+    product
+    |> cast(attrs, [:unit_price])
+    |> validate_required([:unit_price])
+    |> validate_number(:unit_price, greater_than: 0.0)
+    |> validate_number(:unit_price, less_than: old_price)
+  end
 end
